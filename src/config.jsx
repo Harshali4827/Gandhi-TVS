3273f1e52e7cd6b9dc9e8202642d063aa423077f
harshali4827 <harshali4827@gmail.com>
2025-05-31 17:27:07 +0530

    update status in model and export csv branchwise

--- src/config.jsx
+++ src/config.jsx
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 const config = {
-            //    baseURL: import.meta.env.VITE_API_BASE_URL,
-           baseURL: 'http://192.168.1.7:5000/api'
+               baseURL: import.meta.env.VITE_API_BASE_URL,
+        //    baseURL: 'http://192.168.1.7:5000/api'
 };
 
 export default config;
